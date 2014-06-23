3a192afabd658238b8e0984d09cba1f588829b39
Matthew Fisher <matthewf@opdemand.com>
2014-06-23 15:46:53 -0700

    feat(tags): add registry-to-registry transfers
    This new endpoint takes a source registry to pull from and imports the specified
    repository to the current registry. This follows the same syntax as how the docker client
    pulls repositories.
    
    In PaaSLand (both public and private), users want to run their images which are stored
    from their own registries or from DockerHub. Specifically for [Deis](http://deis.io), we
    need a way to be able to pull images from their registry (or DockerHub) over to our
    in-house registry. We make the assumption that every user has access to their own
    registry, so we can't tie down to mirroring from a single remote. The only way we see
    that we can in the current implementation is to:
    
    1. run a docker client
    2. pull the image from the client's registry
    3. push the image to the PaaS' in-house registry (after doing some last-mile layer
       configuration)
    4. pull it down from the host that will be running the application.
    
    This is too slow for our needs and is very inefficient (pull, push, then pull again).
    We'd like to have some way that we can import an image directly from a remote registry.
    This would reduce the processing time significantly.
    
    This commit introduces a new endpoint for the in-house registry:
    
        POST /v1/repositories/<namespace:repository>/tags 'src=example.com:5000/busybox'
    
    This endpoint will pull the repository and all corresponding tags that come with it from
    the remote registry and store it locally. If the repository already exists, then it will
    pull any layers missing from the remote and update the tag metadata to point to the new
    image IDs. This behavior is equivalent to how `docker pull` works.

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -1,9 +1,72 @@
 # -*- coding: utf-8 -*-
 
+import mock
+import requests
+
 import base
 
 from docker_registry.core import compat
 json = compat.json
+
+
+def mock_requests_get_private_registry(path, headers=None):
+    resp = requests.Response()
+    resp.status_code = 200
+    resp._content_consumed = True
+    # resp.headers['X-Fake-Source-Header'] = 'foobar'
+    if path.endswith('0146/layer'):
+        resp._content = "abcdef0123456789xxxxxx=-//"
+    elif path.endswith('0146/json'):
+        resp._content = ('{"id": "cafebabe0146","created":"2014-02-03T16:47:06'
+                         '.615279788Z"}')
+    elif path.endswith('0146/ancestry'):
+        resp._content = '["cafebabe0146"]'
+    elif path.endswith('test/tags'):
+        resp._content = '{"latest": "cafebabe0146", "0.1.2": "cafebabe0146"}'
+    elif path.endswith('test/tags/latest'):
+        resp._content = 'cafebabe0146'
+    elif path.endswith('test/tags/0.1.2'):
+        resp._content = 'cafebabe0146'
+    elif path.endswith('test/images'):
+        resp._content = '[{"id": "cafebabe0146"}]'
+    else:
+        resp.status_code = 404
+
+    return resp
+
+
+def mock_requests_get_public_registry(path, headers=None):
+    """branch logic for DockerHub, as their endpoints are not the same.
+    """
+    resp = requests.Response()
+    resp.status_code = 200
+    resp._content_consumed = True
+    # resp.headers['X-Fake-Source-Header'] = 'foobar'
+    if headers and headers.get('X-Docker-Token') == 'true':
+        resp.headers['x-docker-token'] = 'foobar'
+
+    if path.endswith('asdfqwerty/layer'):
+        resp._content = "abcdef0123456789xxxxxx=-//"
+    elif path.endswith('asdfqwerty/json'):
+        resp._content = ('{"id": "asdfqwerty","created":"2014-02-03T16:47:06'
+                         '.615279788Z"}')
+    elif path.endswith('asdfqwerty/ancestry'):
+        resp._content = '["asdfqwerty"]'
+    elif path.endswith('test/tags'):
+        resp._content = ('['
+                         '{"layer": "asdf", "name": "latest"},'
+                         '{"layer": "asdf", "name": "0.1.2"}'
+                         ']')
+    elif path.endswith('test/tags/latest'):
+        resp._content = '[{"pk": 1234567890, "id": "asdf"}]'
+    elif path.endswith('test/tags/0.1.2'):
+        resp._content = '[{"pk": 1234567890, "id": "asdf"}]'
+    elif path.endswith('test/images'):
+        resp._content = '[{"checksum": "", "id": "asdfqwerty"}]'
+    else:
+        resp.status_code = 404
+
+    return resp
 
 
 class TestTags(base.TestCase):
@@ -128,3 +191,87 @@ class TestTags(base.TestCase):
     def test_special_chars(self):
         repos_name = '{0}%$_-test'.format(self.gen_random_string(5))
         self.test_simple(repos_name)
+
+    @mock.patch('requests.get', mock_requests_get_private_registry)
+    def test_import_repository_from_private_registry(self):
+        data = {
+            'src': 'example.com/othernamespace/test',
+        }
+        resp = self.http_client.post('/v1/repositories/testing2/test/tags',
+                                     data=data)
+        self.assertEqual(resp.status_code, 200)
+        # test that the images were imported
+        resp = self.http_client.get('/v1/images/cafebabe0146/layer')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.data,
+            "abcdef0123456789xxxxxx=-//"
+        )
+        resp = self.http_client.get('/v1/images/cafebabe0146/json')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.data,
+            '{"id": "cafebabe0146","created":"2014-02-03T16:47:06.615279788Z"}'
+        )
+        resp = self.http_client.get('/v1/images/cafebabe0146/ancestry')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.data,
+            '["cafebabe0146"]'
+        )
+        # test that the tags were imported
+        resp = self.http_client.get('/v1/repositories/testing2/test/tags')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            json.loads(resp.data),
+            {"latest": "cafebabe0146", "0.1.2": "cafebabe0146"}
+        )
+        # test that index images were imported
+        resp = self.http_client.get('/v1/repositories/testing2/test/images')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            json.loads(resp.data),
+            [{"id": "cafebabe0146"}]
+        )
+
+    @mock.patch('requests.get', mock_requests_get_public_registry)
+    def test_import_repository_from_public_index(self):
+        data = {
+            'src': 'testing3/test',
+        }
+        resp = self.http_client.post('/v1/repositories/testing3/test/tags',
+                                     data=data)
+        self.assertEqual(resp.status_code, 200)
+        # test that the images were imported
+        resp = self.http_client.get('/v1/images/asdfqwerty/layer')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.data,
+            "abcdef0123456789xxxxxx=-//"
+        )
+        resp = self.http_client.get('/v1/images/asdfqwerty/json')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.data,
+            '{"id": "asdfqwerty","created":"2014-02-03T16:47:06.615279788Z"}'
+        )
+        resp = self.http_client.get('/v1/images/asdfqwerty/ancestry')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            resp.data,
+            '["asdfqwerty"]'
+        )
+        # test that the tags were imported
+        resp = self.http_client.get('/v1/repositories/testing3/test/tags')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            json.loads(resp.data),
+            {"latest": "asdfqwerty", "0.1.2": "asdfqwerty"}
+        )
+        # test that index images were imported
+        resp = self.http_client.get('/v1/repositories/testing3/test/images')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(
+            json.loads(resp.data),
+            [{"id": "asdfqwerty"}]
+        )
